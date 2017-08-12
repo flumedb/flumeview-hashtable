3aa5f0b3415fb10ef8bfef798309cd163906d732
Dominic Tarr <dominic.tarr@gmail.com>
2017-08-12 19:44:39 +1200

    test both random and ordered

--- test/bulk.js
+++ test/bulk.js
@@ -16,16 +16,16 @@ function Timer (name) {
   }
 }
 
-
-var file = '/tmp/test-flumeview-hashtable'+Date.now()+'/log.offset'
-var db =  Flume(
+var create = function (file) {
+  return Flume(
     Log(
       file,
       {blockSize: 1024, codec: require('flumecodec/json')}
     ))
-  .use('index', require('../')(1, hash, null, Math.pow(2, 13)))
+    .use('index', require('../')(1, hash, null, Math.pow(2, 13)))
+}
 
-  var N = 10000
+function initialize (db, N, cb) {
   var data = []
   for(var i = 0; i < N; i++)
     data.push({key: '#'+i, value: {
@@ -33,35 +33,61 @@ var db =  Flume(
     }})
 
   var t = Timer('append')
-
   db.append(data, function (err, offset) {
     //wait until the view is consistent!
     db.index.since(function (v) {
       if(v < offset) return
-
-    t(N)
-    t = Timer('ordered_read')
-      //ordered reads
-      var n = 0
-      for(var i = 0; i < N; i++) {
-        db.index.get('#'+i, next)
-      }
-
-      function next (err, v) {
-        if(++n == N) {
-          t(N)
-          t = Timer('random_read')
-          ;(function get(i) {
-            if(i >= N) {
-              return t(N)
-            }
-            db.index.get(data[~~(Math.random()*N)].key, function (err, data) {
-              setImmediate(function () { get(i+1) })
-            })
-
-          })(0)
-        }
-      }
+      cb(null, N)
     })
   })
+}
+
+function ordered (db, N, cb) {
+  //ordered reads
+  var n = 0
+  for(var i = 0; i < N; i++) {
+    db.index.get('#'+i, next)
+  }
+
+  function next (err, v) {
+    if(++n == N) cb(null, N)
+  }
+}
+
+function random (db, N, cb) {
+  ;(function get(i) {
+    if(i >= N) return cb(null, N)
+
+    db.index.get('#'+~~(Math.random()*N), function (err, data) {
+      setImmediate(function () { get(i+1) })
+    })
+
+  })(0)
+
+}
+
+var file = '/tmp/test-flumeview-hashtable'+Date.now()+'/log.offset'
+
+var db = create(file)
+var N = 10000
+var t = Timer('append')
+initialize(db, N, function (err, n) {
+  t(n)
+  t = Timer('ordered_cached')
+  ordered(db, N, function (err, n) {
+    t(n)
+    t = Timer('random_cached')
+    random(db, N, function (err, n) {
+      t(n)
+      t = Timer('ordered')
+      ordered(create(file), N, function (err, n) {
+        t(n)
+        t = Timer('random_cached')
+        random(create(file), N, function (err, n) {
+          t(n)
+        })
+      })
+    })
+  })
+})
 
