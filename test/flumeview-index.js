df0bfc84888a66927da18bbf8d5b558d06a18b3b
Dominic Tarr <dominic.tarr@gmail.com>
2017-08-12 16:47:27 +1200

    use test-flumeview-index@1

--- test/flumeview-index.js
+++ test/flumeview-index.js
@@ -8,13 +8,14 @@ function hash (v) {
     .digest().readUInt32BE(0)
 }
 
-require('test-flumeview-index')(
-  Flume(
+require('test-flumeview-index')(function (seed) {
+  return Flume(
     Log(
-      '/tmp/test-flumeview-hashtable'+Date.now(),
+      '/tmp/test-flumeview-hashtable'+seed+'/log.offset',
       {blockSize: 1024, codec: require('flumecodec/json')}
     ))
   .use('index', require('../')(1, hash))
-)
+})
+
 
 
